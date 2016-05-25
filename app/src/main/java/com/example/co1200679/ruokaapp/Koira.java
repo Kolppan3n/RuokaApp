9e6f1e839b21ac3b110dee19a4a2bb262d360fdd
Kolppan3n <Kolppan3n@gmail.com>
2016-05-25 13:48:26 +0300

    noniin ja alutus akti on määrätty

---
+++ app/src/main/java/com/example/co1200679/ruokaapp/Koira.java
@@ -0,0 +1,13 @@
+package com.example.co1200679.ruokaapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Koira extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_koira);
+    }
+}
--- app/src/main/java/com/example/co1200679/ruokaapp/MainAct.java
+++ app/src/main/java/com/example/co1200679/ruokaapp/MainAct.java
@@ -1,7 +1,9 @@
 package com.example.co1200679.ruokaapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainAct extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainAct extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void AineksetBtnClick(View view){
+        Intent intentObj = new Intent(this, Ainekset.class);
     }
 }
