9bd783ad50042a65c253b771545001e6c3346a41
co1300608 <antttiniiranen@gmail.com>
2016-05-27 11:03:37 +0300

    Tietokanta toimii.

--- app/src/main/java/com/example/co1200679/ruokaapp/Tietokanta.java
+++ app/src/main/java/com/example/co1200679/ruokaapp/Tietokanta.java
@@ -11,6 +11,8 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class Tietokanta extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "AppiKanta.db";
 
+
+
     public Tietokanta(Context context) {
         super(context, DATABASE_NAME, null, 1);
         SQLiteDatabase db = this.getReadableDatabase();
@@ -18,13 +20,21 @@ public class Tietokanta extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table RuokaKanta (ruokaID INT PRIMARY KEY AUTOINCREMENT) ");
+        db.execSQL("create table RuokaKanta (ruokaID INTEGER PRIMARY KEY AUTOINCREMENT, nimi TEXT NOT NULL,resepti TEXT)");
+        db.execSQL("create table ReseptiKanta (ruokaID INTEGER NOT NULL,kategoriaID INTEGER NOT NULL, aineID INTEGER NOT NULL)");
+        db.execSQL("create table KategoriaKanta (tyyppi TEXT NOT NULL, kategoria TEXT NOT NULL,kategoriaID INTEGER PRIMARY KEY AUTOINCREMENT)");
+        db.execSQL("create table AineKanta (aine TEXT NOT NULL,kategoriaID INTEGER NOT NULL, aineID INTEGER PRIMARY KEY AUTOINCREMENT)");
+
+
     }
 
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS RuokaKanta");
+        db.execSQL("DROP TABLE IF EXISTS ReseptiKanta");
+        db.execSQL("DROP TABLE IF EXISTS AineKanta");
+        db.execSQL("DROP TABLE IF EXISTS KategoriaKanta");
         onCreate(db);
     }
 }
