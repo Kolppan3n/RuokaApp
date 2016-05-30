da7f283de5098e4d86096e88951e0dfb3b4455a9
Kolppan3n <Kolppan3n@gmail.com>
2016-05-30 12:58:01 +0300

    mjoo, itemInfo classi tehty ja xmltyyli itemille

--- app/src/main/java/com/example/co1200679/ruokaapp/Ostoslista.java
+++ app/src/main/java/com/example/co1200679/ruokaapp/Ostoslista.java
@@ -1,10 +1,16 @@
 package com.example.co1200679.ruokaapp;
 
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
@@ -18,7 +24,14 @@ public class Ostoslista extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ostoslista);
-        fillScrollView();
+        toutoutoutoumeitous();
+        //fillScrollView();
+    }
+
+    public void toutoutoutoumeitous(){
+        LinearLayout content = (LinearLayout) findViewById(R.id.content);
+        View temp = getLayoutInflater().inflate(R.layout.item, content, false);
+        content.addView(temp);
     }
 
     //Täyttää listan
@@ -33,8 +46,8 @@ public class Ostoslista extends AppCompatActivity {
         for(int k = 1; k < 31; k++) {
             rnd = new Random();
             color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
-
             temp = new TextView(this);
+
             if(k % 2 == 0) {
                 temp.setText(String.valueOf(k));
                 temp.setBackgroundColor(color);
@@ -47,8 +60,14 @@ public class Ostoslista extends AppCompatActivity {
             temp.setGravity(Gravity.CENTER);
             temp.setTextSize(29);
             temp.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 200));
+
+            temp.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                }
+            });
+
             content.addView(temp);
         }
-
     }
 }
---
+++ app/src/main/java/com/example/co1200679/ruokaapp/itemInfo.java
@@ -0,0 +1,34 @@
+package com.example.co1200679.ruokaapp;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+public class itemInfo extends TextView {
+
+    private String nimi = "Tomaatti";
+
+    public itemInfo(Context context) {
+        super(context);
+        this.setText(getNimi());
+    }
+
+    public itemInfo(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.setText(getNimi());
+    }
+
+    public itemInfo(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        this.setText(getNimi());
+    }
+
+    public String getNimi() {
+        return nimi;
+    }
+
+    public void setNimi(String nimi) {
+        this.nimi = nimi;
+    }
+}
