756a28899d3931516dd704fc5f10e57ee2ad0e83
Kolppan3n <Kolppan3n@gmail.com>
2016-06-03 14:11:45 +0300

    valikko toimii joo joo joo

--- app/src/main/java/com/example/co1200679/ruokaapp/Kokkausohjeet.java
+++ app/src/main/java/com/example/co1200679/ruokaapp/Kokkausohjeet.java
@@ -2,6 +2,7 @@ package com.example.co1200679.ruokaapp;
 
 import android.app.ActionBar;
 import android.database.Cursor;
+import android.database.DatabaseUtils;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
@@ -13,6 +14,8 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class Kokkausohjeet extends AppCompatActivity {
 
@@ -28,47 +31,63 @@ public class Kokkausohjeet extends AppCompatActivity {
         String lause = ("SELECT * FROM RuokaKanta WHERE RuokaID IS " + ruokaID);
         Cursor tiedot = TK.HaeTiedot(lause);
         tiedot.moveToNext();
-        TextView otsikko = (TextView)findViewById(R.id.ruuanNimi);
-        //LinearLayout resepti = (LinearLayout)findViewById(R.id.ohjeKontti);
-        otsikko.setText(tiedot.getString(0));
-        //resepti. tiedot.getString(2);
+        loadRecipe(tiedot);
         fillScrollView();
+    }
+
+    public void loadRecipe(Cursor tiedot){
+        TextView otsikko = (TextView)findViewById(R.id.ruuanNimi);
+        otsikko.setText(tiedot.getString(0));
+        RelativeLayout kontti = (RelativeLayout) findViewById(R.id.ohjeKontti);
+        View temp = getLayoutInflater().inflate(R.layout.resepti, kontti, false);
+        TextView teksti = (TextView) temp.findViewById(R.id.reseptiTeksti);
+        teksti.setText(tiedot.getString(2));
+        kontti.addView(temp);
     }
 
     public void fillScrollView() {
 
         String lause = ("SELECT * FROM AineKanta AK, ReseptiKanta RK WHERE AK.aineID IS RK.aineID AND RK.RuokaID IS " + ruokaID);
 
-        LinearLayout content = (LinearLayout) findViewById(R.id.ainesKontti);
+        LinearLayout kontti = (LinearLayout) findViewById(R.id.ainesKontti);
         View temp;
         ItemInfo item;
-        ImageView icon;
-        RoundImage roi;
 
-        if(content.getChildCount() > 0)
-            content.removeAllViews();
+        if(kontti.getChildCount() > 0)
+            kontti.removeAllViews();
 
         Cursor tiedot = TK.HaeTiedot(lause);
 
+        Log.d("asdasdasd", DatabaseUtils.dumpCursorToString(tiedot));
+
         while(tiedot.moveToNext()) {
-            temp = getLayoutInflater().inflate(R.layout.item, content, false);
-            item = (ItemInfo) temp.findViewById(R.id.itemView);
+            Log.d("dumpaa",tiedot.getString(0));
+            temp = getLayoutInflater().inflate(R.layout.ainesosa, kontti, false);
+            item = (ItemInfo) temp.findViewById(R.id.aine);
             item.setNimi(tiedot.getString(0));
-            icon = (ImageView) temp.findViewById(R.id.imageView);
-            Bitmap bm = BitmapFactory.decodeResource(getResources(),R.drawable.tomaatti);
-            roi = new RoundImage(bm);
-            icon.setImageDrawable(roi);
-            content.addView(temp);
+            kontti.addView(temp);
         }
     }
 
-
-    public void toggleAinekset(View view){
+    public void toggleA(View view){
         LinearLayout lio = (LinearLayout) findViewById(R.id.ainesKontti);
         ViewGroup.LayoutParams lap = lio.getLayoutParams();
 
         if(lap.height == ViewGroup.LayoutParams.WRAP_CONTENT) {
-            lap.height = 0; //new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 100);
+            lap.height = 0;
+            lio.setLayoutParams(lap);
+        } else {
+            lap.height = ViewGroup.LayoutParams.WRAP_CONTENT;
+            lio.setLayoutParams(lap);
+        }
+    }
+
+    public void toggleO(View view){
+        RelativeLayout lio = (RelativeLayout) findViewById(R.id.ohjeKontti);
+        ViewGroup.LayoutParams lap = lio.getLayoutParams();
+
+        if(lap.height == ViewGroup.LayoutParams.WRAP_CONTENT) {
+            lap.height = 0;
             lio.setLayoutParams(lap);
         } else {
             lap.height = ViewGroup.LayoutParams.WRAP_CONTENT;
