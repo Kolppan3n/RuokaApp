74010497b92fa691305bda26a553f6e1a746d10f
Kolppan3n <Kolppan3n@gmail.com>
2016-07-01 12:44:40 +0300

    asetukset toiminnot

--- app/src/main/java/com/example/co1200679/ruokaapp/Asetukset.java
+++ app/src/main/java/com/example/co1200679/ruokaapp/Asetukset.java
@@ -2,12 +2,30 @@ package com.example.co1200679.ruokaapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class Asetukset extends AppCompatActivity {
+
+    Tietokanta TK;
+    Kirjastonhoitaja kirja;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_asetukset);
+        TK = new Tietokanta(this);
+        kirja = new Kirjastonhoitaja(this);
+    }
+
+    public void paivitaTietokanta(View view) {
+        kirja.execute();
+        Toast.makeText(this, "Tietokannat Päivitetty!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void alustaTietokanta(View view) {
+        TK.onUpgrade(TK.getReadableDatabase(), 1, 1); //Tällä saadaan tehtyä uusi versio tietokannasta, mutta tiedot poistu
+        kirja.execute();
+        Toast.makeText(this, "Tietokannat Alustettu!", Toast.LENGTH_SHORT).show();
     }
 }
