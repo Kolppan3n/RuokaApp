f9a17ca99f7424d4b329f30fe0248fd64ebb5e98
co1300608 <antttiniiranen@gmail.com>
2016-07-26 12:46:27 +0300

    Plussa, miinus ja tila poistettu, koska ei enää tarvetta

--- app/src/main/java/com/example/co1200679/ruokaapp/ToolbarFragment.java
+++ app/src/main/java/com/example/co1200679/ruokaapp/ToolbarFragment.java
@@ -170,30 +170,7 @@ public class ToolbarFragment extends Fragment {
                 break;
             }
 
-            case R.id.tila: {
-                Liukuvalikko Lvalikko = (Liukuvalikko) getActivity();
-                RelativeLayout rela = (RelativeLayout) Lvalikko.findViewById(R.id.liukuRela);
 
-                if (Lvalikko.moodi == 0 || Lvalikko.moodi == 1) {
-                    if (Lvalikko.muutaPlussa() == 1) {
-                        item.setIcon(R.drawable.menuselaustila);
-                        rela.setBackgroundColor(getResources().getColor(R.color.colorBackground2));
-                    } else {
-                        item.setIcon(R.drawable.menulisaustila);
-                        rela.setBackgroundColor(getResources().getColor(R.color.colorBackground));
-                    }
-                } else {
-                    if (Lvalikko.muutaPlussa() == 1) {
-                        item.setIcon(R.drawable.menuselaustila);
-                        rela.setBackgroundColor(getResources().getColor(R.color.colorBackground2));
-                    } else {
-                        item.setIcon(R.drawable.menupoistotila);
-                        rela.setBackgroundColor(getResources().getColor(R.color.colorBackground));
-                    }
-                }
-
-                break;
-            }
             case R.id.kokkaus: {
                 Kokkausohjeet kokkausohjeet = (Kokkausohjeet) getActivity();
                 kokkausohjeet.kokkaa();
